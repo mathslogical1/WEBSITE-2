abdf2822c3b1e31f25fcb3451657319989c02f05
Aariz Merchant <merchantaariz@gmail.com>
2020-11-07 19:43:46 +0000

    Added a separate JS File and put the JS in it

---
+++ web2.js
@@ -0,0 +1,6 @@
+alert("Hello there!");
+let apple = prompt("How are you doing?");
+let input = prompt("What's your name?");
+alert("Hi " + input + "!");
+alert("Hope you enjoy my website " + input + "!")
+
